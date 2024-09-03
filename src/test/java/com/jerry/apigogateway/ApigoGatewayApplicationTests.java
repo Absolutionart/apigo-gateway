a63ae5600cfd587de247adac42fa9f040501e328
Absolutionart <719536373@qq.com>
2024-09-03 17:07:39 +0800

    首次提交

---
+++ src/test/java/com/jerry/apigogateway/ApigoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerry.apigogateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigoGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
